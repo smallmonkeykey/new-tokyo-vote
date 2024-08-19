939ac39dbcccc31fdb8797b729456eaa9ab1ed3a
smallmonkeykey <sarinitakuoo@gmail.com>
2024-08-19 11:42:49 +0900

    rubocopし忘れていた

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 class EntriesController < ApplicationController
-  def categories
-  end
+  def categories; end
 
   def new
     category = params[:category]
 
-    if category == 'food'then
+    if category == 'food'
       render 'new_food'
-    elsif category == 'drink' then
+    elsif category == 'drink'
       render 'new_drink'
     end
   end
 
-  def completions
-  end
+  def completions; end
 end
--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -1,17 +1,19 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class EntriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get categories" do
+  test 'should get categories' do
     get entries_categories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get entries_new_url
     assert_response :success
   end
 
-  test "should get completions" do
+  test 'should get completions' do
     get entries_completions_url
     assert_response :success
   end
