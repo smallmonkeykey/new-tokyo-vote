9d8eafd189f978f276c2fbf19b81de34449e45c8
smallmonkeykey <sarinitakuoo@gmail.com>
2024-08-26 18:30:25 +0900

    rubocopし忘れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
