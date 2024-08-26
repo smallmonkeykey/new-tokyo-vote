22409ca575213d17b3825a097304fe1b4fddecf1
smallmonkeykey <sarinitakuoo@gmail.com>
2024-08-26 18:23:26 +0900

    userモデルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :omniauthable
+end
