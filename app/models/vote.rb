a8418df2a958a7f4573ab2b0a3ef6052159d6a82
smallmonkeykey <sarinitakuoo@gmail.com>
2024-09-04 17:24:58 +0900

    rubocopし忘れたため修正

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -8,19 +8,19 @@ class Vote < ApplicationRecord
 
   def self.rankng_by_single(category_name)
     joins(entry: :category)
-    .where(categories: {category_name: category_name})
-    .select('entries.id AS entry_id, COUNT(votes.id) AS votes_count, entries.title AS title, entries.nickname AS nickname')
-    .group('entries.id')
-    .order('votes_count DESC')
-    .limit(3)
+      .where(categories: { category_name: })
+      .select('entries.id AS entry_id, COUNT(votes.id) AS votes_count, entries.title AS title, entries.nickname AS nickname')
+      .group('entries.id')
+      .order('votes_count DESC')
+      .limit(3)
   end
 
   def self.ranking_by_total_votes(category_name)
     joins(entry: :category)
-    .where(categories: {category_name: category_name})
-    .select('entries.id AS entry_id, COUNT(votes.id) AS votes_count, GROUP_CONCAT(DISTINCT entries.title) AS titles, entries.nickname AS nickname')
-    .group('entries.user_id')
-    .order('votes_count DESC')
-    .limit(3)
+      .where(categories: { category_name: })
+      .select('entries.id AS entry_id, COUNT(votes.id) AS votes_count, GROUP_CONCAT(DISTINCT entries.title) AS titles, entries.nickname AS nickname')
+      .group('entries.user_id')
+      .order('votes_count DESC')
+      .limit(3)
   end
 end
