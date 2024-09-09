7d65dce9496b64ad085930ac18ea0debb03b7543
smallmonkeykey <sarinitakuoo@gmail.com>
2024-09-09 21:22:32 +0900

    19時以降は投票を締め切る事とランキングはそれ以降に表示する

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class VotesController < ApplicationController
+  before_action :close_voting, only: [:create]
   def categories; end
 
   def completions; end
@@ -19,5 +20,13 @@ class VotesController < ApplicationController
 
   def vote_params
     params.require(:vote).permit(:comment, :entry_id)
+  end
+
+  def close_voting
+    closing_time = Time.new(2024, 9, 29, 19, 0o0, 0, '+09:00')
+
+    return unless Time.current >= closing_time
+
+    redirect_to rankings_path, alert: '19時以降は投票できません'
   end
 end
