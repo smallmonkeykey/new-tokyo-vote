ebe833af0a6022d328a3564425b7bff8c674f67a
smallmonkeykey <sarinitakuoo@gmail.com>
2024-09-09 18:16:35 +0900

    日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module TokyuVote2
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/jp.yml').to_s]
