03e027609520f2e77b496c0b752760be85f98281
smallmonkeykey <sarinitakuoo@gmail.com>
2024-09-24 11:37:36 +0900

    投票数と何に投票したかを表示する

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module VotesHelper
+  MAX_VOTES_PER_CATEGORY = 3
+
+  def remaind_vote_count(category_name)
+    votes_count = current_user.votes.joins(entry: :category).where(category: { category_name: }).count
+    MAX_VOTES_PER_CATEGORY - votes_count
+  end
+
+  def voted_entries(category_name)
+    votes = current_user.votes.includes(entry: :user).joins(entry: :category).where(category: { category_name: })
+    votes.map do |vote|
+      {
+        title: vote.entry.title,
+        user_name: vote.entry.user.name
+      }
+    end
+  end
 end
