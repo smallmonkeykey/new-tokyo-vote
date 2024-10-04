aa83fcf7071200a5bba3cb58ecfd14fbbab0033f
smallmonkeykey <sarinitakuoo@gmail.com>
2024-10-04 21:56:29 +0900

    OGP関連を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  # def ogp_meta_tags
-  #   {
-  #     og: {
-  #       site_name: 'TokyuVote',
-  #       title: 'TokyuVote',
-  #       description: 'TokyuRubykaigi15で使う投票アプリです',
-  #       type: 'website',
-  #       url: request.original_url,
-  #       image: image_url('tokyu.png'),
-  #       locale: 'ja_JP'
-  #     }
-  #   }
-  # end
 end
