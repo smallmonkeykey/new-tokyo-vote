7a20d12f2ef4b1ade2ff2809539d1f1d184558f4
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-18 16:31:01 +0900

    管理者のTOPページを作成

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,6 +2,11 @@
 
 module Admin
   class EventsController < ApplicationController
+    include AdminHelper
+    before_action :require_admin
 
+    def index
+      @events = Event.order(created_at: :desc)
+    end
   end
 end
