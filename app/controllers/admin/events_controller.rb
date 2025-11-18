2e324fecd2367129dec026e510e72fee0bd837f1
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-18 20:47:29 +0900

    更新ボタンの設置と日本語に対応

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,7 +4,7 @@ module Admin
   class EventsController < ApplicationController
     include AdminHelper
     before_action :require_admin
-    before_action :set_event, only: [:show, :update]
+    before_action :set_event, only: %i[show update]
 
     def index
       @events = Event.order(created_at: :desc)
@@ -19,14 +19,28 @@ module Admin
 
         @rankings[category] = {
           single: votes.ranking_by_single,
-          total:  votes.ranking_by_total_votes
+          total: votes.ranking_by_total_votes
         }
       end
     end
 
+    def update
+      if @event.update(event_params)
+        redirect_to admin_event_path(@event), notice: 'ステータスを更新しました'
+      else
+        flash.now[:alert] = 'ステータスの更新に失敗しました'
+        render :show, status: :unprocessable_entity
+      end
+    end
+
     private
+
     def set_event
       @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:status)
     end
   end
 end
