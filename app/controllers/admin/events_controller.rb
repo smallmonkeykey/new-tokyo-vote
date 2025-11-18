15c9b06eb03f0c6a47ada1045f9fda67cab7c9b8
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-18 18:58:05 +0900

    管理者はいつでも結果をみれるページを作成

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,9 +4,29 @@ module Admin
   class EventsController < ApplicationController
     include AdminHelper
     before_action :require_admin
+    before_action :set_event, only: [:show, :update]
 
     def index
       @events = Event.order(created_at: :desc)
+    end
+
+    def show
+      categories = %w[food drink lt]
+      @rankings = {}
+
+      categories.each do |category|
+        votes = Vote.for_event(@event.id).for_category(category)
+
+        @rankings[category] = {
+          single: votes.ranking_by_single,
+          total:  votes.ranking_by_total_votes
+        }
+      end
+    end
+
+    private
+    def set_event
+      @event = Event.find(params[:id])
     end
   end
 end
